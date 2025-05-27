32c26ceb50c075da198d721aef3c8aaa8812bf0a
Jack Francis Dalton <jackfrancisdalton@gmail.com>
2025-05-27 21:21:14 +0100

    chore: added takeaway section

--- covariant-arrays/main.ts
+++ covariant-arrays/main.ts
@@ -6,3 +6,11 @@ const n: number = nums[3]; // 🌟 Qurik: we can assign this value to a number c
 
 // Now what appears to be a number is actually a string, but TypeScript does not complain about it!
 console.log(n);
+
+
+// 🧠 TAKEAWAY:
+// - TypeScript allows assigning a more specific type (like number[]) to a more general type (like any[]), which can lead to unexpected behaviour.
+// - This is because TypeScript uses structural typing, meaning it checks the shape of the types rather than their explicit type.
+// - When we assign a value from a more general type (like any[]) to a more specific type (like number), TypeScript does not check the actual value at runtime, 
+// - leading to potential runtime errors
+// AVOID ANY!
